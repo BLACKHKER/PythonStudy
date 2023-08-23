b40719a6357b8e097bbd5a2beccaf090a6259779
blackhker <bhkdos@gmail.com>
2023-08-23 14:06:28 +0800

    if流程控制

---
+++ study/Include/com.bhk/study/If.py
@@ -0,0 +1,20 @@
+# if
+# age = 24
+# if age < 25:
+#     print("Success!")
+
+# if-else
+# age = 25
+# if age < 25:
+#     print("Success!")
+# else:
+#     print("Error!")
+
+# elif
+age = 25
+if age < 25:
+    print("lt!")
+elif age > 25:
+    print("gt!")
+else:
+    print("True!")
