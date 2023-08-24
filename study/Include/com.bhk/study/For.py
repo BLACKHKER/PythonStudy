8fc2b548d7fedf9bbcdc04174597ffb2bf7eb95d
blackhker <bhkdos@gmail.com>
2023-08-24 17:07:43 +0800

    for循环

---
+++ study/Include/com.bhk/study/For.py
@@ -0,0 +1,13 @@
+# for循环Demo
+# array = [1, 2, 3, 4, 5]
+# for i in array:
+#     print(i)
+# print("Success!")
+
+# 判断字符串中有几个a
+# num = 0
+# str = "panda is big animal"
+# for i in str:
+#     if (i == "a"):
+#         num += 1
+# print(f"该字符串中有{num}个a字符")
