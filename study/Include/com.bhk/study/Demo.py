6dc17217c2ded0818d859d1e325cb9be34db0405
blackhker <bhkdos@gmail.com>
2023-08-24 18:27:32 +0800

    for循环、range、random、流程控制的发工资Demo

---
+++ study/Include/com.bhk/study/Demo.py
@@ -0,0 +1,19 @@
+import random
+
+"""
+    公司账户余额有一万元，给20名员工发工资，员工没人领取工资1000元
+    财务需要判断该员工的绩效(随机数)，低于5不发工资
+    工资发完则结束发工资
+"""
+money = 10000
+for i in range(1, 21):
+    if money >= 1000:
+        score = random.randint(1, 10)
+        if score > 5:
+            money -= 1000
+            print(f"向员工{i}发放工资1000元，余额{money}元")
+        else:
+            print(f"员工{i},绩效分{score},低于5,不发工资！")
+    else:
+        print("没钱了！")
+        break
