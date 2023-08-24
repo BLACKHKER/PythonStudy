af1b6d0712acc39b2d730d4838b696a8875abc25
blackhker <bhkdos@gmail.com>
2023-08-24 17:23:37 +0800

    range()等差序列集合生成

---
+++ study/Include/com.bhk/study/Api.py
@@ -0,0 +1,29 @@
+"""
+    range()
+"""
+# 从0开始，到num结束
+# numsArray = range(10)
+# for i in numsArray:
+#     print(i)
+
+# 从num1开始，到num2结束，不含num2本身
+# numsArray = range(10,18)
+# for i in numsArray:
+#     print(i)
+
+# 从num1开始，到num2结束，步长为num3
+# numArray = range(4,10,2)
+# for i in numArray:
+#     print(i)
+
+# 统计有几个偶数
+# x = 1
+# y = 100
+# count = 0
+# numsArray = range(x, y)
+# for i in numsArray:
+#     if (i % 2 == 0):
+#         count += 1
+# print(f"{x}到{y}有{count}个偶数")
+
+"""==============================================================================================="""
