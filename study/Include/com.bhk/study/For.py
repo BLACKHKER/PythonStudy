2cca45b3f060139cc9641050a94556e6f363a698
blackhker <bhkdos@gmail.com>
2023-08-24 17:57:43 +0800

    for循环九九乘法表

--- study/Include/com.bhk/study/For.py
+++ study/Include/com.bhk/study/For.py
@@ -13,6 +13,14 @@
 # print(f"该字符串中有{num}个a字符")
 
 # 注意：for循环的临时变量是可以被循环体外部访问到的如下代码会打印两遍4
-for i in range(5):
-    print(i)
-print(i)
+# i = 0
+# for i in range(5):
+#     print(i)
+# print(i)
+
+# 九九乘法表
+for i in range(1, 10):
+    for j in range(1, 10):
+        if j <= i:
+            print(f"{j}*{i}={i * j}\t", end='')
+    print()
