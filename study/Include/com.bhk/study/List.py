8a843b07101250b7f857a4268a90e93470bbfe2b
blackhker <bhkdos@gmail.com>
2023-08-25 11:13:05 +0800

    列表(JavaArray+List)的方法

---
+++ study/Include/com.bhk/study/List.py
@@ -0,0 +1,39 @@
+'''
+@Project ：PyCharmWorkSpace 
+@File    ：List.py
+@Author  ：BLACKHKER
+@Date    ：2023/8/25 10:14 
+'''
+
+'''
+    获取索引下标为0的元素
+'''
+# myList = [1, 2, 3, 4, 5]
+# 正向索引
+# print(myList[0])
+# 反向索引
+# print(myList[-1])
+
+'''
+    遍历矩阵
+'''
+# myList2 = [[1, 2, 3], [4, 5, 6]]
+# for i in myList2:
+#     for j in i:
+#         print(j)
+# 获取矩阵的元素
+# print(myList2[0][1])
+
+'''
+    插入元素
+'''
+myList = [1, 2, 3, "3"]
+# 在列表的末尾新增元素
+# myList.append(1)
+
+'''
+    统计元素个数
+'''
+# print(myList.count(3))
+
+print(myList)
