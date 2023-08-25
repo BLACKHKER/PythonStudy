37e27381cd46c26f978b791c5346a1444c76872a
blackhker <bhkdos@gmail.com>
2023-08-25 18:11:57 +0800

    文件IO

---
+++ study/Include/com.bhk/study/Open.py
@@ -0,0 +1,10 @@
+'''
+@Author  ：BLACKHKER
+@Date    ：2023/8/25 17:53
+@File    ：Open.py
+@Description: 文件操作(JavaIO流)
+@Version 1.0
+'''
+
+# 以只读方式打开文件
+f = open("D:\logs\exportData.txt", r, encoding="UTF-8")
